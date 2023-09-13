83d2b07c795a994797f58c7e1e617cdeeb72dc68
Onesimus Chandra Pradhan <onesimus.pradhan@cosmotech.com.ph>
2023-09-13 18:00:38 +0530

    updates

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New Empty is created
+// 2. Function is called, this = {}
+// 3. {} linked to prototype
+// 4. Function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const Jack = new Person('Jack', 1975);
+console.log(matilda, Jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype;
