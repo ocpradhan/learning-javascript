378b7f0511f2231adb2a398d43f2a9e94e613e7e
ocpradhan <ocpradhan@gmail.com>
2023-12-01 14:02:19 +0530

    updates

---
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+// cloneDeep
+
+const state = {
+  cart: [
+    {product: "bread", quantity: 5},
+    {product: "pizza", quantity: 5},
+  ],
+  user: {loggedIn: true},
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
@@ -41,3 +42,65 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+// import { addToCart } from './shoppingCart';
+
+/*
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 5);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+*/
+
+/*
+// Export 
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (shipping is ${shippingCost})`
+  );
+};
+
+// Import
+const {addToCart} = require('./shoppingCart.js');
+*/
+
+import cloneDeep from '/./node_modules/lodash-es/cloneDeep.js';
+
+// cloneDeep
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+console.log(stateClone);
